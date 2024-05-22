ada61c0e1bc4567ac3d4c68a5275f4815debf85c
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2024-05-23 03:40:06 +0530

    Files recovered

---
+++ script.js
@@ -0,0 +1,28 @@
+const id1 = Math.floor(Math.random()*25 +1);
+console.log(id1);
+
+const id2 = Math.floor(Math.random()*25 +1);
+console.log(id2);
+
+const id3 = Math.floor(Math.random()*25 +1);
+console.log(id3);
+
+const boy = document.querySelector('div');
+
+function check(id){
+    const box = document.getElementById(`${id}`);
+    box.style.backgroundColor = "#8f6ddbc2";
+
+    if(id == id1 || id == id2 || id == id3){
+        box.style.backgroundImage = "url('mine.Bomb-X0T.svg')";
+
+        setInterval(() => {
+            boy.innerHTML = '<h1> Game Over Please refresh for replay </h1> ';
+        }, 1000);  
+    }
+    else{
+        // console.log("NOT MAth");
+        box.style.backgroundImage = "url('dimond-BH.svg')"
+        box.style.backgroundRepeat = "no-repeat"
+    }
+}
