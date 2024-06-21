b1fe529aba0a23c6ce831273fd550f86bc5c0626
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2024-06-21 10:59:44 +0530

    ////

--- src/script.js
+++ src/script.js
@@ -1,13 +1,13 @@
 
 
 const id1 = Math.floor(Math.random()*25 +1);
-console.log(id1);
+// console.log(id1);
 
 const id2 = Math.floor(Math.random()*25 +1);
-console.log(id2);
+// console.log(id2);
 
 const id3 = Math.floor(Math.random()*25 +1);
-console.log(id3);
+// console.log(id3);
 
 const boy = document.querySelector('div');
 
@@ -15,16 +15,18 @@ document.querySelectorAll('div[id]').forEach((id) => {
     id.addEventListener('click', (e) => {
         const id = e.target.id;
         const box = document.getElementById(`${id}`);
-        box.style.backgroundColor = "#8f6ddbc2";
-
+        
         if(id == id1 || id == id2 || id == id3){
+            box.style.backgroundColor = "#8f6ddbc2";
             box.style.backgroundImage = "url('../images/mine.Bomb-X0T.svg')";
 
             setInterval(() => {
                 boy.innerHTML = '<h1> Game Over Please refresh for replay </h1> ';
             }, 700);  
         }
+
         else{
+            box.style.backgroundColor = "#8f6ddbc2";
             // console.log("NOT MAth");
             box.style.backgroundImage = "url('../images/dimond-BH.svg')"
             box.style.backgroundRepeat = "no-repeat"
