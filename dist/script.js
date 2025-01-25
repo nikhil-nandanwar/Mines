06c7c5a126ff9465f450e63b2b610b7f72598c69
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2025-01-26 03:20:31 +0530

    intend

--- dist/script.js
+++ dist/script.js
@@ -7,13 +7,13 @@ let mines = selectOptions.value;
 
 pickRandomBoxes();
 
-selectOptions.addEventListener('change' , (e) => {
-    if(!clicked ){
+selectOptions.addEventListener('change', (e) => {
+    if (!clicked) {
         mines = e.target.value;
         arr = [];
         pickRandomBoxes();
     }
-    else{
+    else {
         const dialoge = document.getElementById('interrupt');
         dialoge.showModal();
         dialoge.style.display = 'block';
@@ -21,10 +21,10 @@ selectOptions.addEventListener('change' , (e) => {
 });
 
 function pickRandomBoxes() {
-    let num ;
-    while(arr.length != mines){
-        num = Math.floor(Math.random() *25 +1);
-        if(!arr.includes(num)){
+    let num;
+    while (arr.length != mines) {
+        num = Math.floor(Math.random() * 25 + 1);
+        if (!arr.includes(num)) {
             arr.push(num);
         }
     }
@@ -33,18 +33,18 @@ function pickRandomBoxes() {
 
 document.querySelectorAll('div[id]').forEach((divs) => {
     // console.log(divs);
-    divs.addEventListener('click' , (e) => {
+    divs.addEventListener('click', (e) => {
         clicked = true;
         // console.log(e);
         const id = e.target.id;
         const box = document.getElementById(`${id}`);
         // console.log(box);
-        if(arr.indexOf(Number(id)) === -1){
+        if (arr.indexOf(Number(id)) === -1) {
             box.style.backgroundColor = "#8f6ddbc2";
             box.style.backgroundImage = "url('./images/dimond-BH.svg')";
             box.style.backgroundRepeat = "no-repeat";
         }
-        else{
+        else {
             box.style.backgroundColor = "#8f6ddbc2";
             box.style.backgroundImage = "url('./images/mine.Bomb-X0T.svg')";
             setInterval(() => {
@@ -57,16 +57,16 @@ document.querySelectorAll('div[id]').forEach((divs) => {
 })
 
 
-document.getElementById('refreshButton').addEventListener('click' , () => {
+document.getElementById('refreshButton').addEventListener('click', () => {
     console.log("hello");
     location.reload();
 });
-document.getElementById('refreshButtonInterrupt').addEventListener('click' , () => {
+document.getElementById('refreshButtonInterrupt').addEventListener('click', () => {
     console.log("hello");
     location.reload();
 });
 
-document.getElementById('continue').addEventListener("click" , () => {
+document.getElementById('continue').addEventListener("click", () => {
     const dialoge = document.getElementById('interrupt');
     dialoge.close();
     dialoge.style.display = 'none';
--- dist/selectForm.js
+++ dist/selectForm.js
@@ -1,10 +1,10 @@
 const select = document.querySelector('select');
 
-for(let i=1 ; i<=25 ; i++){
+for (let i = 1; i <= 25; i++) {
     const option = document.createElement('option');
     option.value = i;
     option.innerText = i;
-    if(i == 3){
+    if (i == 3) {
         option.selected = true;
     }
     select.appendChild(option);
